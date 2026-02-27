6115d8583e1c1e7ad716d32a23f2b2d514a41fac
Mohini <ondoordairydeveloper@gmail.com>
2026-02-27 16:30:07 +0530

    final commit

--- server.js
+++ server.js
@@ -19,8 +19,34 @@ app.get("/", (req, res) => {
   res.send("Backend Running 🚀");
 });
 
-const port = process.env.PORT || 5000;
+let isConnected = false;
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port} 🔥`);
-});
+    isConnected = true;
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+// add middleware
+app.use((req, res,next) => {
+  if (!isConnected) {
+    connectToMongoDB()
+  }
+  next()
+})
+
+
+
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port} 🔥`);
+// });
+module.exports = app
